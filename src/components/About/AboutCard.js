d3aeda15270321eb6faa137ace43b3f469c1a817
Shaurya Tiwari <shauryatiwari@wc-dhcp51d237.employee-secure.wireless.fsu.edu>
2024-01-23 13:36:53 -0500

    Update Info

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,22 +9,21 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Shaurya Tiwari </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Tallahassee, Florida</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently pursuing my Masters in Computer Science and working part time at Center of Oceanic and Atmospheric Prediction Science as the lead web Developer.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed my Bachelors in Technology in the field of Information Technology.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Jiu-jitsu
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Boxing
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -32,9 +31,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "If you are not failing enough, you are not trying enough."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Elon Musk</footer>
         </blockquote>
       </Card.Body>
     </Card>
